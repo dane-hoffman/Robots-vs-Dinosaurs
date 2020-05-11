339789f36cc5d50a3dc0ed7f7c6017dba356803d
unknown <dane.hoffman90@gmail.com>
2020-05-11 09:42:53 -0600

    Created Class objects; 'Robot', 'Dinosaur', 'Fleet', 'Herd', 'Weapon', and 'Battlefield'

--- RoboVsDino/Robot.cs
+++ RoboVsDino/Robot.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace RoboVsDino
 {
-    class Program
+    class Robot
     {
-        static void Main(string[] args)
-        {
-                  
-
-        }
     }
 }
--- RoboVsDino/Weapon.cs
+++ RoboVsDino/Weapon.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace RoboVsDino
 {
-    class Program
+    class Weapon
     {
-        static void Main(string[] args)
-        {
-                  
-
-        }
     }
 }
